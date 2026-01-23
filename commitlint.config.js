917cbf994d5fd0251e828325bdaa942587282f47
Lucas Miranda <ldemattosmiranda@gmail.com>
2026-01-22 21:27:25 -0300

    chore: configuração commilint (#4)
    - adiciona configuração do commitlint;
    - configura commit-msg;

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-"export default { extends: ['@commitlint/config-conventional'] };" 
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "chore", "docs", "refactor", "test"],
+    ],
+    "subject-empty": [2, "never"],
+    "subject-case": [0], // não forçar case
+  },
+};
